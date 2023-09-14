d62e50b3b7f0d7f0c4a070183ec157f5b502eef7
rabelenda <rabelenda@gmail.com>
2023-09-14 13:19:18 -0300

    Fix update of sample project

--- .github/update-sample-version.sh
+++ .github/update-sample-version.sh
@@ -10,7 +10,7 @@ USER_EMAIL="$(git log --format='%ae' HEAD^!)"
 USER_NAME="$(git log --format='%an' HEAD^!)"
 
 cd jmeter-dotnet-dsl-sample
-sed -i 's/Include="Abstracta.JmeterDsl" Version="[0-9.]\+"/Include="Abstracta.JmeterDsl" Version="${VERSION}"/g' Abstracta.JmeterDsl.Sample/Abstracta.JmeterDsl.Sample.csproj
+sed -i "s/Include=\"Abstracta.JmeterDsl\" Version=\"[0-9.]\+\"/Include=\"Abstracta.JmeterDsl\" Version=\"${VERSION}\"/g" Abstracta.JmeterDsl.Sample/Abstracta.JmeterDsl.Sample.csproj
 
 git add .
 git config --local user.email "$USER_EMAIL"
